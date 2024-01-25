4551fc853f1d6badec58c569b077f9442ac51157
Hengki <47341624+hengki-irawan@users.noreply.github.com>
2024-01-25 07:31:16 +0100

    test

--- webpage.py
+++ webpage.py
@@ -14,21 +14,9 @@ LOTTIE_ANIMATION_ABOUTME = load_animation(lottie_aboutme_url)
 # Main Header
 st.set_page_config(page_title="Hengki's gallery", page_icon=Image.open(f"{PAGE_ICON}"))
 st.subheader("Silent Frames")
-st.title("Capturing Quiet Moments: Through My Lens")
-# local_css("style/form_style.css") #to hide streamlit brand
+st.title(":blue[Capturing Quiet Moments: Through My Lens]")
+local_css("style/form_style.css") #to hide streamlit brand
 
-st.markdown("""
-<style>
-
-stTitle>title {
-color: rgb(251,190,91);     
-}            
-
-                        
-.stButton>button[type=submit]:hover {
-  background-color: #1e9ae0;
-}
-</style>""", unsafe_allow_html=True)
 
 # Buttons
 with st.container():
